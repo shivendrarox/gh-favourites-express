30845f3db4ea76e4f7ef4503a8e1308728cf49ba
shivendrarox <32977980+shivendrarox@users.noreply.github.com>
2022-05-23 15:41:59 +0530

    making a search endpoint

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const bodyParser = require("body-parser");
 const app = express()
 const fileUpload = require('express-fileupload');
 const port = 3000
+const { Octokit, App } = require("octokit");
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // enable files upload
 app.use(fileUpload());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.post('/generate-settings', (req, res) => {
     let settingsFromUser=req.body;
@@ -44,6 +52,20 @@ app.post('/generate-settings', (req, res) => {
         res.status(500).send(err);
     }
   })
+
+  app.post('/search', async (req, res) => {
+          console.log(req.body.searchTerm)
+
+        const octokit = new Octokit({
+                auth: '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'
+              })
+              
+            let dataFromGh = await octokit.request(`GET /search/repositories?q=${req.body.searchTerm}&per_page=10`, {})
+
+   console.log(dataFromGh.data)
+      res.json(dataFromGh.data)
+  })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
