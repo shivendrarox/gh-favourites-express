97d61ff6eafdb5c22cc0282145b8e5a0fa172d38
shivendrarox <32977980+shivendrarox@users.noreply.github.com>
2022-05-24 00:42:23 +0530

    polishing

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(fileUpload());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://gh-search-three.vercel.app"); // or "http://localhost:3000" 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
@@ -32,34 +32,24 @@ app.post('/generate-settings', (req, res) => {
                 message: 'No file uploaded'
             });
         } else {
-            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+
             let settingsFromFile = req.files.settingsfile;
 
             //send response
             res.json(JSON.parse(settingsFromFile.data.toString()))
-            
-            // res.send({
-            //     status: true,
-            //     message: 'File is uploaded',
-            //     data: {
-            //         name: avatar.name,
-            //         mimetype: avatar.mimetype,
-            //         size: avatar.size
-            //     }
-            // });
         }
     } catch (err) {
         res.status(500).send(err);
     }
   })
 
+        const octokit = new Octokit({
+                auth: '--------------------'
+              })
+              
   app.post('/search', async (req, res) => {
           console.log(req.body.searchTerm)
 
-        const octokit = new Octokit({
-                auth: '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'
-              })
-              
             let dataFromGh = await octokit.request(`GET /search/repositories?q=${req.body.searchTerm}&per_page=10`, {})
 
    console.log(dataFromGh.data)
